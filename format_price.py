05a42d0b6aef741f3bba5a8a7d704f5a0d8372f2
romanyakovlev <yakovlevroman8@gmail.com>
2016-12-24 02:51:55 +0700

    добавил всю логику для скрипта а также тесты

--- format_price.py
+++ format_price.py
@@ -1,5 +1,52 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('number')
+    number_value = parser.parse_args().number
+    return number_value
+
+
 def format_price(price):
-    pass
+    price_string = try_to_float(price)
+    formatted_price_string = []
+    left_side_string, right_side_string = price_string.split('.')[0], price_string.split('.')[1]
+    left_side = make_left_side(left_side_string)
+    formatted_price_string.append(left_side)
+    is_all_zeros = sum(1 for x in right_side_string if x is '0') is len(right_side_string)
+    if not is_all_zeros:
+        formatted_price_string.append(right_side_string)
+    return '.'.join(formatted_price_string)
+
+
+def try_to_float(price):
+    try:
+        price_string = str(float(price))
+        return price_string
+    except ValueError:
+        print(" '{}' не является допустимым значением.".format(price))
+        quit()
+
+
+def make_left_side(left_side_string):
+    amount_of_three_nums_through_space = len(left_side_string) // 3
+    whole_numbers_amount = amount_of_three_nums_through_space * 3
+    if amount_of_three_nums_through_space:
+        nums_through_space_list = []
+        after_three_nums = left_side_string[:-whole_numbers_amount]
+        if after_three_nums:
+            nums_through_space_list += after_three_nums
+        three_nums_through_space_string = left_side_string[-whole_numbers_amount:]
+        three_nums_through_space_list = [three_nums_through_space_string[x*3: x*3 + 3]
+                                                                for x in range(amount_of_three_nums_through_space)]
+        nums_through_space_list += three_nums_through_space_list
+        return ' '.join(nums_through_space_list)
+    else:
+        nums_without_space = left_side_string
+        return nums_without_space
+
 
 if __name__ == '__main__':
-    pass
+    number_value = parse_arguments()
+    print(format_price(number_value))
--- tests.py
+++ tests.py
@@ -1,1 +1,31 @@
-# TODO place your tests here
+import unittest
+import sys
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_zero_value(self):
+        formated_price = format_price('0')
+        self.assertEqual(formated_price, '0')
+        formated_price = format_price('0.00')
+        self.assertEqual(formated_price, '0')
+
+    def test_spaces(self):
+        formated_price = format_price('1234567890')
+        self.assertEqual(formated_price, '1 234 567 890')
+        formated_price = format_price('1234')
+        self.assertEqual(formated_price, '1 234')
+        formated_price = format_price('123')
+        self.assertEqual(formated_price, '123')
+
+    def test_zeros_after_the_decimal_point(self):
+        formated_price = format_price('1.10000')
+        self.assertEqual(formated_price, '1.1')
+        formated_price = format_price('1.0100')
+        self.assertEqual(formated_price, '1.01')
+        formated_price = format_price('0.01')
+        self.assertEqual(formated_price, '0.01')
+
+
+if __name__ == '__main__':
+    unittest.main()
